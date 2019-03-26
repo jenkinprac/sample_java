823c7fc12f54708455548a61d708fe76b8b1fb1f
rahil <rahil@samsung.com>
2019-03-26 07:53:40 +0000

    code remodified

--- sample.java
+++ sample.java
@@ -2,6 +2,8 @@ public class sample
 {
 	public static void main(String[] args) {
 		System.out.println("Java Code under construction"); 
+		System.out.println("Java Crashed our production server"); 
+
 	}
 }
 
